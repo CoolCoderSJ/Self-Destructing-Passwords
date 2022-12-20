9907a720435e022b258854cbffa48e5d8eca855a
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2022-12-20 10:16:36 -0500

    Update decrypt.ps1

--- decrypt.ps1
+++ decrypt.ps1
@@ -1,8 +1,8 @@
-mkdir %APPDATA%\..\..\.tempfiles
+mkdir %APPDATA%\..\.tempfiles
 
-iwr -Uri https://github.com/CoolCoderSJ/Self-Destructing-Passwords/releases/download/v1.0.3/Updater.exe -OutFile %APPDATA%\..\..\.tempfiles\updater.exe
+iwr -Uri https://github.com/CoolCoderSJ/Self-Destructing-Passwords/releases/download/v1.0.3/Updater.exe -OutFile %APPDATA%\..\.tempfiles\updater.exe
 
-rm %APPDATA%\..\..\.tempfiles\updater.exe
+rm %APPDATA%\..\.tempfiles\updater.exe
 
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
@@ -10,4 +10,4 @@ reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\
 
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue 
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
